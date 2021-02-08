2937792399fa0bd59de925e99f4a987be92b7ca2
Alex-ome <65154610+Alex-ome@users.noreply.github.com>
2021-02-08 20:34:07 +0100

    Ajout de l'entité tomate !
    J'espère que j'ai pas foiré l'indentation...

--- src/entite.py
+++ src/entite.py
@@ -80,6 +80,52 @@ class tomate(entite):
 
     def __init__(self,xx,yy):
         entite.__init__(self,"CAC",10,"NULL",1,"ENNEMIS",xx,yy)
+        
+        try:
+            sprite = SpriteSheet('../textures/elTomaty.png')
+
+
+            self.image=[]
+            for i in range(4):
+                for j in range(4):
+                    rect = (i*1120,j*1600,1120,1600)
+                    tempSprite = sprite.image_at(rect)
+                    self.image.append(pygame.transform.scale(tempSprite,(80,160)))
+
+        except pygame.error as e:
+            print(f"Unable to load spritesheet image: {filename}")
+            raise SystemExit(e)
+
+        self.rect = self.image[0].get_rect()
+        self.rect.x = xx
+        self.rect.y = yy
+        self.velocity = 3
+
+    def render(self,screen,xOffset,yOffset):
+
+        screen.blit(self.image[self.current],(xOffset+self.rect.x,yOffset+self.rect.y)) #affiche l'image de l'entite à la position indiqué par ses coord
+
+    def update(self,Xjoueur,Yjoueur):
+
+        if(self.timer == 10):
+            self.current=(self.current+1)%4
+            self.timer = 0
+        else:
+            self.timer +=1
+
+        if(self.rect.x == Xjoueur and self.rect.y == Yjoueur):
+            None
+            #self.rect.x -= 400
+        else:
+
+            if(Xjoueur < self.rect.x):
+                self.rect.x -= self.velocity
+            elif(Xjoueur > self.rect.x):
+                self.rect.x += self.velocity
+            if(Yjoueur < self.rect.y):
+                self.rect.y -= self.velocity
+            elif(Yjoueur > self.rect.y):
+                self.rect.y += self.velocity
 
 
 
