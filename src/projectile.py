6bb3c19960761c239a9e246b295494723f9961aa
Alex-ome <65154610+Alex-ome@users.noreply.github.com>
2021-02-09 18:55:10 +0100

    Update projectile.py

--- src/projectile.py
+++ src/projectile.py
@@ -36,7 +36,7 @@ class Projectile(pygame.sprite.Sprite):
     def render(self,screen,xOffset,yOffset):
         if(self.exist != False):
             screen.blit(self.image[self.current],(xOffset+self.rect.x,yOffset+self.rect.y))
-            self.update("aaa")
+            
 
     def update(self,entities):
         #for entities check si touché ou non temp = entitie touché
@@ -56,7 +56,21 @@ class Projectile(pygame.sprite.Sprite):
     #check si une entite est touché
     #Renvois l'entité si touché, False sinon
     def doesHit(self,entitie):
-        return False
+        if self.hasHit == False :
+            ennemisTouches = pygame.sprite.spritecollide(self, entitie, False)
+            if ennemisTouches :
+                for entite in ennemisTouches:
+                    entite.vie -= self.damage
+                    if entite.vie <= 0:
+                        entite.killed = True
+                        print("Dead")
+
+                return True
+            else:
+
+                return False
+        else:
+            return True
     
     def move(self):
         if(self.direction == "up"):
