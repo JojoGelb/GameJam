d223b83732a02661d517a3c1166a6826cced357d
houny <julien.houny@etu.univ-grenoble-alpes.fr>
2021-02-10 16:07:49 +0100

    Sprite gérer

--- src/entite.py
+++ src/entite.py
@@ -4,7 +4,7 @@ import random
 
 class entite(pygame.sprite.Sprite):
 
-    def __init__(self,typeAttaque,vie,projectile,degats,typpe,xx,yy):
+    def __init__(self,typeAttaque,vie,projectile,degats,typpe,xx,yy,sprite):
         super().__init__()
         self.vie=vie
         self.typeAttaque=typeAttaque
@@ -20,32 +20,14 @@ class entite(pygame.sprite.Sprite):
         self.hitbox=None
         self.alreadyKilled = False
         self.exist = True
-                
+        self.image = sprite        
 
 class carotte(entite):
 
-    def __init__(self,xx,yy):
-        entite.__init__(self,"CAC",10,"NULL",1,"ENNEMIS",xx,yy)
+    def __init__(self,xx,yy,sprite,death):
+        entite.__init__(self,"CAC",10,"NULL",1,"ENNEMIS",xx,yy,sprite)
     
-    
-    
-        #sprites :
-        self.spriteDeath = SpriteSheet('../textures/Carottedodo.png') 
-        try:
-            sprite = SpriteSheet('../textures/Carotte.png')
-
-
-            self.image=[]
-            for i in range(2):
-                for j in range(2):
-                    rect = (i*416,j*1024,416,1024)
-                    tempSprite = sprite.image_at(rect)
-                    self.image.append(pygame.transform.scale(tempSprite,(64,160)))
- 
-
-        except pygame.error as e:
-            print(f"Unable to load spritesheet image: {filename}")
-            raise SystemExit(e)
+        self.imageMort = death
 
         
         #or de la carotte :
@@ -119,18 +101,11 @@ class carotte(entite):
             pygame.mixer.Sound.play(death_sound)
             self.alreadyKilled = True
             self.timer = 0
-            self.current = 0
+            self.current = 3
 
-        self.image=[]
-        for i in range(4):
-                rect = (0,i*1024,416,1024)
-                tempSprite = self.spriteDeath.image_at(rect)
-                self.image.append(pygame.transform.scale(tempSprite,(64,160)))
-        
         self.timer += 1
+        self.image = self.imageMort
         self.image[self.current].set_alpha(200-self.timer)
-        if(self.timer%10 == 0 and self.current < 3 ):
-                self.current+=1
         if(self.timer >=50):
             self.exist = False
             
@@ -139,32 +114,15 @@ class carotte(entite):
 class tomate(entite):
 
 
-    def __init__(self,xx,yy):
-        entite.__init__(self,"CAC",10,"NULL",1,"ENNEMIS",xx,yy)
+    def __init__(self,xx,yy,sprite):
+        entite.__init__(self,"CAC",10,"NULL",1,"ENNEMIS",xx,yy,sprite)
         
-        #sprites :
-        
-        try:
-            sprite = SpriteSheet('../textures/elTomaty.png')
-
-
-            self.image=[]
-            for i in range(14):
-                rect = (i*350,0,350,500)
-                tempSprite = sprite.image_at(rect)
-                self.image.append(pygame.transform.scale(tempSprite,(128,256)))
-
-        except pygame.error as e:
-            print(f"Unable to load spritesheet image: {filename}")
-            raise SystemExit(e)
-
-
         #or de la tomate :
         self.gold=10
 
         #hit box ( anciennement rect )
         self.hitbox = self.image[0].get_rect()
-        self.hitbox.height = self.hitbox.height-150
+        self.hitbox.height = self.hitbox.height-100
         self.hitbox.x = xx
         self.hitbox.y = yy
         #NE PAS UTILISER RECT POUR AUTRE CHOSE QUE LA GESTION DE COLLISION  + masque
