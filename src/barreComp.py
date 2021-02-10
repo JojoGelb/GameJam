7957e5e72c0b370ec5c95ba7d4251ab121da0820
Sevan Mulin <mulinsevan@orange.fr>
2021-02-10 21:29:47 +0100

    barredevieok

--- src/barreComp.py
+++ src/barreComp.py
@@ -10,7 +10,7 @@ class barreComp(pygame.sprite.Sprite):
 
 
 
-    def __init__(self,screenWidth,screenHeight):
+    def __init__(self,screenWidth,screenHeight,vie):
         super().__init__()        
         try:
             self.image=pygame.transform.scale(pygame.image.load('../textures/barreComp.png'),(int(screenWidth*0.85),int(screenHeight*0.15))) 
@@ -26,17 +26,21 @@ class barreComp(pygame.sprite.Sprite):
         self.murmurT = smallfont.render("50g   [E]" , True , (250,250,250))
         self.morClicable = True
         self.murClicable = True
-
+        self.VieMax=vie
         self.screenHeight=screenHeight
         self.screenWidth=screenWidth
 
-    def render(self,screen):
+    def render(self,screen,vie):
 
         screen.blit(self.image, self.pos)
         screen.blit(self.murmur,(int(self.screenWidth*0.12),int(self.screenHeight*0.89)))
         screen.blit(self.murmurT,(int(self.screenWidth*0.12),int(self.screenHeight*0.92)))
         screen.blit(self.mormor,(int(self.screenWidth*0.29),int(self.screenHeight*0.89)))
         screen.blit(self.mormorT,(int(self.screenWidth*0.29),int(self.screenHeight*0.92)))
+        viepourc=100-int(((vie*100)/self.VieMax))
+        print(viepourc)
+        pygame.draw.rect(screen,(41,41,41),(self.screenWidth*0.90-viepourc*2.4,self.screenHeight*0.88,viepourc*2.4,70))
+
         
     def update(self,gold):
         
